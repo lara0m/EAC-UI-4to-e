c0eeb4e1b87b968b91cc346d17c70a561d9a6eb5
Lara Muiña <152939155+lara0m@users.noreply.github.com>
2025-05-14 15:55:45 -0300

    ejercicio par o impar hecho

---
+++ Assets/PARIMPAR.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PARIMPAR : MonoBehaviour
+{
+    public int num1;
+    public TMP_InputField inputNum;
+    public TextMeshProUGUI txtResultado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultado.text = "";
+    }
+    public void ParImpar()
+    {
+        num1 = int.Parse(inputNum.text);
+        if(num1 <= 0)
+        {
+            txtResultado.text = "El número debe ser mayor a 0";
+            return;
+
+        } else if(num1 % 2 == 0)
+        {
+            txtResultado.text = "El número es par";
+        } else
+        {
+            txtResultado.text = "El número es impar";
+        }
+        
+    }
+
+}
